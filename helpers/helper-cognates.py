75c9f6aa46c637403c4433eb222a703f2e947f07
LinguList <mattis.list@uni-marburg.de>
2016-07-26 13:22:56 +0200

    upate data

--- helpers/helper-cognates.py
+++ helpers/helper-cognates.py
@@ -8,3 +8,17 @@ except:
     lex.output('tsv', filename='../bins/Dogon-227-20')
 
 lex.cluster(method='lexstat', threshold=0.6, cluster_method='infomap')
+lex.output('tsv', filename='../data/Dogon-227-20-cognates.tsv', ignore='all',
+        prettify=False)
+
+# plot a nj tree
+from lingpy.convert.plot import plot_tree
+lex.calculate('tree', ref='lexstatid', tree_calc='neighbor')
+plot_tree(lex.tree, fileformat='pdf', filename='../plots/dogon-tree', degree=355)
+
+alm = Alignments(lex, ref='lexstatid')
+alm.align()
+alm.output('tsv', filename='../data/Dogon-227-20-alignments.tsv', ignore='all',
+        prettify=False)
+
+

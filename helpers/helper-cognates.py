f7c645d67deff5d56b1cb1609c1b48059f373fec
LinguList <mattis.list@uni-marburg.de>
2016-07-26 13:45:45 +0200

    final corrs

--- helpers/helper-cognates.py
+++ helpers/helper-cognates.py
@@ -7,8 +7,8 @@ except:
     lex.get_scorer(runs=10000)
     lex.output('tsv', filename='../bins/Dogon-227-20')
 
-lex.cluster(method='lexstat', threshold=0.6, cluster_method='infomap')
-lex.output('tsv', filename='../data/Dogon-227-20-cognates.tsv', ignore='all',
+lex.cluster(method='lexstat', threshold=0.55, cluster_method='infomap')
+lex.output('tsv', filename='../data/Dogon-227-20-cognates', ignore='all',
         prettify=False)
 
 # plot a nj tree
@@ -17,8 +17,9 @@ lex.calculate('tree', ref='lexstatid', tree_calc='neighbor')
 plot_tree(lex.tree, fileformat='pdf', filename='../plots/dogon-tree', degree=355)
 
 alm = Alignments(lex, ref='lexstatid')
-alm.align()
-alm.output('tsv', filename='../data/Dogon-227-20-alignments.tsv', ignore='all',
-        prettify=False)
+alm.align(scoredict=lex.cscorer, method='library')
+alm.output('tsv', filename='../data/Dogon-227-20-alignments', ignore='all',
+        prettify=False, subset=True, cols=[c for c in sorted(alm.header,
+            key=lambda x: alm.header[x]) if c != 'concept_list'])
 
 
